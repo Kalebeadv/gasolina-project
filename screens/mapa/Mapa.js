9940be301bcf136d47377df53df0fd2085d64399
kalebe <kalebeadv66@gmail.com>
2022-03-29 00:36:25 -0300

    adicionando mapa

--- screens/mapa/Mapa.js
+++ screens/mapa/Mapa.js
@@ -1,77 +1,98 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, StyleSheet } from "react-native";
-import MapView from 'react-native-maps';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import config from "../../config/googleConfig.json";
+import * as React from "react"
+import { Dimensions, StyleSheet, Text, View } from "react-native"
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
+import MapView, { Callout, Circle, Marker } from "react-native-maps"
 
 
 
 export default function Mapa() {
 
-  const [origin, setOrigin] = useState(null);
-  const [destination, setDestination] = useState(null);
+  const [ pin, setPin ] = React.useState({
+		latitude: 37.78825,
+		longitude: -122.4324
+	})
+	const [ region, setRegion ] = React.useState({
+		latitude: 37.78825,
+		longitude: -122.4324,
+		latitudeDelta: 0.0922,
+		longitudeDelta: 0.0421
+	})
 
-  useEffect(() => {
-    (async function () {
-      const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION_FOREGROUND);
-      if (status === 'granted') {
-        let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
-        setOrigin({
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-          latitudeDelta: 0.000922,
-          longitudeDelta: 0.000421
-        })
-      } else {
-        throw new Error('Location permission not granted');
-      }
-    })();
-  }, []);
-
-  return (
-    <View style={styleMapa.container}>
-      <MapView style={styleMapa.map}
-        initialRegion={origin}
-        showsUserLocation={true}
-        zoomEnabled={false}
-        loadingEnabled={true}
-      >
-      </MapView>
-      <View style={StyleSheet.search}>
-        <GooglePlacesAutocomplete
-          placeholder='Para onde vamos?'
-          onPress={(data, details = null) => {
-            // 'details' is provided when fetchDetails = true
-            console.log(data, details);
-          }}
-          query={{
-            key: config.googleMapKey,
-            language: 'pt-br',
-          }}
-
-          enablePoweredByContainer={false}
-          fetchDetails={true}
-          styles={{ listView: { height: 100 } }}
-        />
-      </View>
-    </View>
-  )
+	return (
+		<View style={{ marginTop: 50, flex: 1 }}>
+			<GooglePlacesAutocomplete
+				placeholder="Search"
+				fetchDetails={true}
+				GooglePlacesSearchQuery={{
+					rankby: "distance"
+				}}
+				onPress={(data, details = null) => {
+					// 'details' is provided when fetchDetails = true
+					console.log(data, details)
+					setRegion({
+						latitude: details.geometry.location.lat,
+						longitude: details.geometry.location.lng,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421
+					})
+				}}
+				query={{
+					key: "AIzaSyDkPz3CZtdL0jjmvHU0FQap1s7ktTwvWrM",
+					language: "pt-br",
+					components: "country:br",
+					types: "establishment",
+					radius: 30000,
+					location: `${region.latitude}, ${region.longitude}`
+				}}
+				styles={{
+					container: { flex: 0, position: "absolute", width: "100%", zIndex: 1 },
+					listView: { backgroundColor: "white" }
+				}}
+			/>
+			<MapView
+				style={styles.map}
+				initialRegion={{
+					latitude: 37.78825,
+					longitude: -122.4324,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421
+				}}
+				provider="google"
+			>
+				<Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
+				<Marker
+					coordinate={pin}
+					pinColor="black"
+					draggable={true}
+					onDragStart={(e) => {
+						console.log("Drag start", e.nativeEvent.coordinates)
+					}}
+					onDragEnd={(e) => {
+						setPin({
+							latitude: e.nativeEvent.coordinate.latitude,
+							longitude: e.nativeEvent.coordinate.longitude
+						})
+					}}
+				>
+					<Callout>
+						<Text>I'm here</Text>
+					</Callout>
+				</Marker>
+				<Circle center={pin} radius={1000} />
+			</MapView>
+		</View>
+	)
 }
 
-
-const styleMapa = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    backgroundColor: '#fff',
-    justifyContent: "flex-start",
-  },
-  map: {
-    height: "70%"
-  },
-  search: {
-    height: 50
-  }
-});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	map: {
+		width: Dimensions.get("window").width,
+		height: Dimensions.get("window").height
+	}
+})

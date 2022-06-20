aaf3089d391ee07cd5cff43cfe1baa182be177b5
kalebe <kalebeadv66@gmail.com>
2022-06-20 02:03:09 -0300

    esse login maldito esta bugado, estou ficando louco

---
+++ Login/script.js
@@ -0,0 +1,18 @@
+var usuarios = [
+    {"login": "kalebe", "senha": "tf2"},
+    {"login": "carlos", "senha": "elas"},
+    {"login": "gustavo", "senha": "123"},
+];
+
+function entrar_() {
+    var usuario = document.getElementById('username').value.toLowerCase();
+    var senha = document.getElementById('password').value;
+    
+    for (var u in usuarios) {
+        var us = usuarios[u];
+        if (us.login === usuario && us.senha === senha) {
+           window.location.href = "Admin-site/index.html"
+        }
+    }
+    alert("Dados incorretos, tente novamente.");
+}
